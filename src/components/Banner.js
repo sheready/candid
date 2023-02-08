5c2a612a33350835e37920269584cbc0413e85bc
sheready <csstlw@gmail.com>
2023-02-08 14:38:31 +0300

    add header and video

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import './style/Banner.css';
-import img1 from '../media/images/img1.JPG'
-import img2 from '../media/images/img2.JPG'
-import img3 from '../media/images/img3.JPG'
+import src from '../media/video/ciwb.mp4'
 
 
 const Banner = () => {
   return (
-    <div>
-        
+    <div className="header-column">
+        <div id="banner-text">
+          <h3>WELCOME TO</h3>
+          <h1>CANDID CONVERSATIONS</h1>
+          <h2>CIRCLES FOR WOMEN IN BUSINESS</h2>
+        </div>
+        <div id="banner-video">
+          <video controls height='350px' autoplay muted loop>
+            <source src={src} type="video/mp4" />
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+        </div>
+
         
     </div>
   )
