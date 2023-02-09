9d253b91695c4847329b09de86ff72cf387b7063
sheready <csstlw@gmail.com>
2023-02-09 15:48:52 +0300

    edit autoPlay settings

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,20 +5,21 @@ import src from '../media/video/ciwb.mp4'
 
 const Banner = () => {
   return (
-    <div className="header-column">
-        <div id="banner-text">
+    <div>
+      <div class="row">
+        <div class="col-sm-8" id="banner-text">
           <h3>WELCOME TO</h3>
-          <h1>CANDID CONVERSATIONS</h1>
+          <h1>CANDID</h1>
+          <h1>CONVERSATIONS</h1>
           <h2>CIRCLES FOR WOMEN IN BUSINESS</h2>
         </div>
-        <div id="banner-video">
-          <video controls height='350px' autoplay muted loop>
+        <div class="col-sm-4" id="banner-video">
+          <video autoPlay loop muted>
             <source src={src} type="video/mp4" />
             Sorry, your browser doesn't support embedded videos.
           </video>
         </div>
-
-        
+      </div>
     </div>
   )
 }
