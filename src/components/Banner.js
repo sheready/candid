b2db7549f21c435f0d23dad16f3e4aa900bb3cc7
sheready <csstlw@gmail.com>
2023-02-11 02:30:16 +0300

    add a banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 import './style/Banner.css';
-import banner2 from '../media/images/banner2.png'
-
+import logo from '../media/images/logo.png'
 
 const Banner = () => {
   return (
-    <>
-    <div>
-      
-    </div>
-      <div id="banner-image">
-        <img src={banner2} class="d-block w-100" alt="..."/>
-
+    <div class="banner-container">
+      <div class="banner-text">
+        <h1>WELCOME TO</h1>
+        <h1>CANDID CONVERSATIONS</h1>
+        <h3>CIRCLES FOR WOMEN IN BUSINESS</h3>
       </div>
-    </>
-    
-  )
+      <div class="banner-image">
+        <img src={logo} alt="banner"/>
+      </div>
+    </div>
+
+  );
 }
 
-export default Banner
+export default Banner;
