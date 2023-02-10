702ab3226f3482b3af11d881142fe17f1d2b0232
sheready <csstlw@gmail.com>
2023-02-10 22:50:46 +0300

    add banner image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import './style/Banner.css';
-import src from '../media/video/ciwb.mp4'
+import banner2 from '../media/images/banner2.png'
 
 
 const Banner = () => {
   return (
+    <>
     <div>
-      <div class="row">
-        <div class="col-sm-8" id="banner-text">
-          <h3>WELCOME TO</h3>
-          <h1>CANDID</h1>
-          <h1>CONVERSATIONS</h1>
-          <h2>CIRCLES FOR WOMEN IN BUSINESS</h2>
-        </div>
-        <div class="col-sm-4" id="banner-video">
-          <video autoPlay loop muted>
-            <source src={src} type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </div>
-      </div>
+      
     </div>
+      <div id="banner-image">
+        <img src={banner2} class="d-block w-100" alt="..."/>
+
+      </div>
+    </>
+    
   )
 }
 
