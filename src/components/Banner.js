9bf270b5e40fa710de0f326adc0c5abcb851e122
sheready <csstlw@gmail.com>
2023-02-20 18:07:49 +0300

    add banner background image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,11 +7,8 @@ const Banner = () => {
     <div class="banner-container">
       <div class="banner-text">
         <h1>WELCOME TO</h1>
-        <h1>CANDID CONVERSATIONS</h1>
+        <h1 id="main-header">CANDID CONVERSATIONS</h1>
         <h3>CIRCLES FOR WOMEN IN BUSINESS</h3>
-      </div>
-      <div class="banner-image">
-        <img src={logo} alt="banner"/>
       </div>
     </div>
 
