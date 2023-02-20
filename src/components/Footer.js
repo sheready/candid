95fa77c1d24caf751e6ca58388d8592d0d98883e
sheready <csstlw@gmail.com>
2023-02-20 18:09:07 +0300

    add logo on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,22 @@
 import React from 'react'
 import './style/Footer.css'
+import logo from '../media/images/logo.png'
 
 const Footer = () => {
   return (
         <section id="footer">
             <div class="container">
                 <div class="row text-center text-xs-center text-sm-left text-md-left">
-                    <div class="col-xs-12 col-sm-4 col-md-4">
-                        <h5>Quick links</h5>
+                    <div class="col-xs-12 col-sm-6 col-md-6">
                         <ul class="list-unstyled quick-links">
                             <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
                             <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
                             <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Join Us As A Retailer</a></li>
                         </ul>
                     </div>
-                    <div class="col-xs-12 col-sm-4 col-md-4">
-                        <h5>Quick links</h5>
-                        <ul class="list-unstyled quick-links">
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-4 col-md-4">
-                        <h5>Quick links</h5>
-                        <ul class="list-unstyled quick-links">
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-                            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-                            <li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
-                        </ul>
+                    <div class="col-xs-12 col-sm-4 col-md-4" id="logo-footer">
+                        <img src={logo} alt="logo" />
                     </div>
                 </div>
                     <hr></hr>
