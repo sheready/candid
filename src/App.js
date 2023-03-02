028705ac7853ec40a7da968597eb1415eef92bfd
sheready <csstlw@gmail.com>
2023-03-02 16:29:28 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import Banner from './components/Banner';
 import Header from './components/Header';
-import Cards from './components/Cards';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
 
-    <Header/>
-    <Banner/>
-    <Cards />
-    <Footer/>
- 
- 
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route
+          path="*"
+          element={<h1 className="not-found">Page Not Found</h1>}
+        />
+      </Routes>
+       
+      <Footer/>
+
     </div>
     
   );
