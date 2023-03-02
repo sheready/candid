2d8a41deca9df381b488d5385ff36a84f9cc8592
sheready <csstlw@gmail.com>
2023-03-02 16:32:47 +0300

    add contact us component

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style/Contact.css';
+
+const Contact = () => {
+  return (
+    <div class="contact-container">
+      <div class="contact-text">
+        <h1 id="contact-header">CONTACT US</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
