1767444415835f61a1475b7313abf496faa86511
sheready <csstlw@gmail.com>
2023-03-02 16:32:15 +0300

    add about us component

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style/About.css'
+
+const About = () => {
+  return (
+    <>
+    <div class="about-container">
+      <div class="about-text">
+        <h1 id="about-header">ABOUT US</h1>
+      </div>
+    </div>
+    <h4 id="topic-header">ABOUT US</h4>
+    </>
+  )
+}
+
+export default About
