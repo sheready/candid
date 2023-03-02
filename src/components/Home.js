743de067b7761d9817ab93e3c21192b6966209ab
sheready <csstlw@gmail.com>
2023-03-02 16:33:33 +0300

    refractor app.js

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './Banner';
+import Cards from './Cards';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <Cards/>
+    </div>
+  )
+}
+
+export default Home
